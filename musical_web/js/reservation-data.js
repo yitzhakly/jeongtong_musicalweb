971dd6dbf6d2df93b8dbfb7b644ab7103f5a9bf1
yitzhak <awayomelas@gmail.com>
2025-06-20 22:28:29 +0900

    이전 공연 추가

---
+++ musical_web/js/reservation-data.js
@@ -0,0 +1,282 @@
+window.CURRENT = [
+    {
+      title : '스웨그에이지 외쳐, 조선!',
+      poster: '../img/po-swagage.gif',
+      synop : '왜 운명은 스스로 만들 수 없는가',
+      period: '2025.06.20 - 2025.08.31',
+      venue : '홍익대 대학로 아트센터 대극장',
+      cast  : '양희준·임규형·김수하 외',
+      link  : 'https://tickets.interpark.com/goods/25006099'
+    },
+    {
+      title : '팬텀',
+      poster: '../img/po-phantom.gif',
+      synop : '그대는 내게로 와 나의 음악이 되리라',
+      period: '2025.05.31 - 2025.08.11',
+      venue : '세종문화회관 대극장',
+      cast  : '박효신·카이·전동석·이지혜 외',
+      link  : 'https://tickets.interpark.com/goods/P0004249?app_tapbar_state=hide&'
+    },
+    {
+      title : '강호석 이상하다',
+      poster: '../img/po-kang.jpg',
+      synop : '할 말이 없는데,,,,',
+      period: '2008.02.26 - 2866.13.68',
+      venue : '수원시 영통구 ',
+      cast  : '엑스트라',
+      link  : 'https://solved.ac/profile/suhodnkla'
+    },
+      {
+        title : '브로드웨이 42번가',
+        poster: '../img/po-42nd.gif',
+        synop : '그래도, 전 제 꿈을 포기하지 않을 거에요',
+        period: '2025.07.10 - 2025.09.14',
+        venue : '샤롯데씨어터',
+        cast  : '박칼린·박건형·양준모·정영주·윤공주·장지후·기세중 외',
+        link  : 'https://tickets.interpark.com/goods/25007451?app_tapbar_state=hide&#'
+      },
+      {
+        title : '이석민',
+        poster: '../img/po-stone.jpg',
+        synop : '안녕하세요 저는 빠른 돌멩이입니다. 저는 과거 짱돌 출신이지만 과거를 청산했습니다.',
+        period: '2008.03.28 - 2020.02.02',
+        venue : '화성시 동탄 2',
+        cast  : '아하하',
+        link  : 'https://www.instagram.com/seokmin._.0328?igsh=MjJqb3kzMGtzNjR2'
+      },
+      {
+        title : '매디슨 카운티의 다리',
+        poster: '../img/po-dari.gif',
+        synop : '우린 순간을 영원으로 바꿀거야',
+        period: '2025.05.01 - 2025.07.13',
+        venue : '광림아트센터 BBCH홀',
+        cast  : '박은태·최재림·조정은·차지연 외',
+        link  : 'https://tickets.interpark.com/goods/25003119?app_tapbar_state=hide&'
+      },
+      {
+          title : '맘마미아!',
+          poster: '../img/po-mam.gif',
+          synop : '행복해지는 주문 맘마미아!',
+          period: '2025.07.26 - 2025.10.25',
+          venue : 'LG아트센터 서울 LG SIGNATURE 홀',
+          cast  : '최정원·신영숙·루나·최태이·홍지민 외',
+          link  : 'https://tickets.interpark.com/goods/L0000123?app_tapbar_state=hide&#'
+        },
+        {
+          title : '멤피스',
+          poster: '../img/po-mem.gif',
+          synop : '나의 Memphis 이곳에 영원토록 살아 숨쉰다',
+          period: '2025.06.17 - 2025.09.21',
+          venue : '충무아트센터 대극장',
+          cast  : '박강현·고은성·정택운·이창섭·정선아 외',
+          link  : 'https://tickets.interpark.com/goods/25005076?app_tapbar_state=hide&#'
+        },
+        {
+          title : '빨래',
+          poster: '../img/po-bbal.gif',
+          synop : '빨래가 바람에 제 몸을 맡기는 것 처럼 인생도 바람에 맡기는 거야',
+          period: '2025.03.12 - 2025.09.21',
+          venue : 'NOL 유니플렉스 2관',
+          cast  : '김도원·기영수·오주언·문남권·류석호 외',
+          link  : 'https://tickets.interpark.com/goods/25002027?app_tapbar_state=hide&#'
+        },
+        {
+          title : '오늘 밤, 세계에서 이 사랑이 사라진다 해도',
+          poster: '../img/po-tonight.gif',
+          synop : '내일 모든 걸 잊는다 해도 가장 행복한 오늘을 줄게!',
+          period: '2025.06.13 - 2025.08.24',
+          venue : '코엑스 신한카드 아티움',
+          cast  : '윤소호·장민제 외',
+          link  : 'https://tickets.interpark.com/goods/25006181?app_tapbar_state=hide&'
+        },
+        {
+          title : '차미',
+          poster: '../img/po-cha.gif',
+          synop : '이 세상은 완성품을 원해',
+          period: '2025.05.29 - 2025.08.24',
+          venue : '대학로 TOM 1관',
+          cast  : '홍나현·이봄소리 외',
+          link  : 'https://tickets.interpark.com/goods/25005848?app_tapbar_state=hide&'
+        },
+        {
+          title : '베어 더 뮤지컬',
+          poster: '../img/po-bare.gif',
+          synop : '네가 곧 문제고 또 답인 걸 알아 ',
+          period: '2025.06.03 - 2025.09.14',
+          venue : '두산아트센터 연강홀',
+          cast  : '강병훈·홍기범 외',
+          link  : 'https://tickets.interpark.com/goods/25005242?app_tapbar_state=hide&#'
+        },
+        {
+          title : '프리다',
+          poster: '../img/po-prida.gif',
+          synop : '나도 알아 굿바이 키스를 보낼게',
+          period: '2025.06.17 - 2025.09.07',
+          venue : 'NOL 유니플렉스 1관',
+          cast  : '김소향·김히어라 외',
+          link  : 'https://tickets.interpark.com/goods/25006050?app_tapbar_state=hide&'
+        },
+        {
+          title : '두 낫 디스터브',
+          poster: '../img/po-donot.gif',
+          synop : '이 칼이 세상을 순결케 하리니',
+          period: '2025.06.10 - 2025.08.31',
+          venue : 'NOL 서경스퀘어 스콘 1관',
+          cast  : '김지온·최호승 외',
+          link  : 'https://tickets.interpark.com/goods/25006546#'
+        },
+        {
+          title : '다시, 동물원',
+          poster: '../img/po-re.gif',
+          synop : '흐린 가을 하늘에 편지를 써',
+          period: '2025.06.26 - 2025.09.14',
+          venue : '동덕여자대학교 공연예술센터 코튼홀',
+          cast  : '정욱진·오승윤 외',
+          link  : 'https://tickets.interpark.com/goods/25007139?app_tapbar_state=hide&#'
+        },
+        {
+          title : '구텐버그',
+          poster: '../img/po-99.gif',
+          synop : '울면 안 돼 절망 속에서 희망을 꿈꿔요',
+          period: '2025.04.30 - 2025.07.20',
+          venue : '플러스씨어터',
+          cast  : '조풍래·장지후 외',
+          link  : 'https://tickets.interpark.com/goods/25004494?app_tapbar_state=hide&#'
+        },
+        {
+          title : '쇼맨_어느 독재자의 네 번째 대역배우',
+          poster: '../img/po-show.gif',
+          synop : '인생은 내 키만큼 깊은 바다',
+          period: '2025.07.11 - 2025.08.31',
+          venue : '국립정동극장',
+          cast  : '윤나무·강기둥 외',
+          link  : 'https://tickets.interpark.com/goods/25007456?app_tapbar_state=hide&#'
+        },
+        {
+          title : '등등곡',
+          poster: '../img/po-ddg.gif',
+          synop : '천하여! 내 어찌 흘러갈지 지켜보겠다',
+          period: '2025.06.24 - 2025.09.14',
+          venue : '예스24스테이지 1관',
+          cast  : '김재범·고상호 외',
+          link  : 'https://tickets.interpark.com/goods/25006845?app_tapbar_state=hide&#'
+        },
+        {
+          title : '마리퀴리',
+          poster: '../img/po-mari.gif',
+          synop : '내게 언제나 넌 크고 환한 별',
+          period: '2025.07.25 - 2025.10.19',
+          venue : '광림아트센터 BBCH홀',
+          cast  : '김소향·옥주현 외',
+          link  : 'https://tickets.interpark.com/goods/25008162?app_tapbar_state=hide&#'
+        },
+        {
+          title : '스트라빈스키',
+          poster: '../img/po-stra.gif',
+          synop : '문제는 너의 음악',
+          period: '2025.07.28 - 2025.10.12',
+          venue : '대학로 TOM 2관',
+          cast  : '임준혁·서영택 외',
+          link  : 'https://tickets.interpark.com/goods/25008305?app_tapbar_state=hide&#'
+        },
+        {
+          title : '와일드 그레이',
+          poster: '../img/po-wild.gif',
+          synop : '우리의 현실은 바라던 이야기로 남아',
+          period: '2025.05.21 - 2025.08.17',
+          venue : '링크아트센터 벅스홀',
+          cast  : '홍기범·정휘 외',
+          link  : 'https://www.ticketlink.co.kr/product/55835'
+        },
+        {
+          title : '낙원',
+          poster: '../img/po-nak.jpg',
+          synop : '나의 겨울은 널 위한 봄',
+          period: '2025.07.15 - 2025.10.12',
+          venue : '링크아트센터드림 드림3관',
+          cast  : '박새힘·이아진 외',
+          link  : 'https://www.ticketlink.co.kr/product/56994'
+        },
+        {
+          title : '백작',
+          poster: '../img/po-100.jpg',
+          synop : '나는 나를 용서한다, 나는 나를 해방한다, 나는 나를 석방한다',
+          period: '2025.04.16 - 2025.07.07',
+          venue : '링크아트센터드림 드림1관',
+          cast  : '박경호·이승현 외',
+          link  : 'https://www.ticketlink.co.kr/product/55400'
+        },
+        {
+          title : '이매지너리',
+          poster: '../img/po-ima.jpg',
+          synop : '나누고 더할게 많은 우리 일상',
+          period: '2025.04.15 - 2025.07.06',
+          venue : '링크아트센터드림 드림3관',
+          cast  : '임준혁·방진의 외',
+          link  : 'https://www.ticketlink.co.kr/product/55104'
+        },{
+          title : '트레드밀',
+          poster: '../img/po-tred.gif',
+          synop : '네가 선택한 트레드밀 앞으로 가는 게 아냐',
+          period: '2025.07.15 - 2025.10.12',
+          venue : '링크아트센터드림 드림1관',
+          cast  : '반정모·정동화 외',
+          link  : 'https://www.ticketlink.co.kr/product/56983'
+        },
+        {
+          title : '마하고니',
+          poster: '../img/po-maha.gif',
+          synop : '끊임없는 놀이와 허리케인 속에서 길을 잃고 말 것인가',
+          period: '2025.06.17 - 2025.08.31',
+          venue : '링크아트센터 페이코홀',
+          cast  : '최재웅·이아름솔 외',
+          link  : 'https://www.ticketlink.co.kr/product/56465'
+        },
+        {
+          title : '더 크리처',
+          poster: '../img/po-thecri.gif',
+          synop : '진흙으로 나를 빚어 사람으로 제발 만들어 달라고 청했습니까',
+          period: '2025.06.16 - 2025.08.31',
+          venue : '대학로 자유극장',
+          cast  : '박민성·문태유 외',
+          link  : 'https://www.ticketlink.co.kr/product/56118'
+        },
+        {
+          title : '올랜도 in 버지니아',
+          poster: '../img/po-all.jpg',
+          synop : '타는 불꽃으로. 그 고통과 갈망으로',
+          period: '2025.07.09 - 2025.10.09',
+          venue : '링크아트센터드림 드림4관',
+          cast  : '임찬민·김이후 외',
+          link  : 'https://www.ticketlink.co.kr/product/56769'
+        },
+        {
+          title : '민들레 피리',
+          poster: '../img/po-min.jpg',
+          synop : '동경, 그리고 만주에서 서로와 조국의 안녕을 물었던 수십 장의 편지와 시',
+          period: '2025.06.15 - 2025.09.07',
+          venue : '예스24스테이지 2관',
+          cast  : '박영수·손유동 외',
+          link  : 'http://ticket.yes24.com/Perf/53690?Gcode=009_403'
+        },{
+          title : '붉은 정원',
+          poster: '../img/po-red.gif',
+          synop : '당신이 주는 아픔은 왜 이리 달콤할까요',
+          period: '2025.04.22 - 2025.07.20',
+          venue : '링크아트센터드림 드림2관',
+          cast  : '강정우·조성윤 외',
+          link  : 'https://tickets.interpark.com/goods/25002866#'
+        }
+  
+      
+    ];
+    
+    window.PREVIOUS = [
+        { title:'긴긴밤',    poster:'../img/po-long.gif'     },
+        { title:'더데빌', poster:'../img/po-the.gif' },
+        { title:'유진과 유진', poster:'../img/po-ujin.gif' },
+        { title:'미스터 마우스', poster:'../img/po-mr.gif' },
+        { title:'미드나잇 액터뮤지션', poster:'../img/po-mid.gif' },
+        { title:'넥스트 투 노멀', poster:'../img/po-next.gif' },
+        { title:'쓰릴 미', poster:'../img/po-th.gif' },
+      ];
